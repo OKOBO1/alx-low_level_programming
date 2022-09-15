6f66a01b9c5cad360a2c63fcf35820d7be146511
OKOBO1 <ugofabian01@gmail.com>
2022-09-15 15:56:57 -0700

    More functions, more nested loops in C

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * print_line - function that checks for uppercase character.
- * @n: is the integer for the paramaters of my function
- * Return: 0
+ * print_line - draws a straight line in the terminal
+ *@n: number of times to print the line
+ * Return: Always 0.
  */
+
 void print_line(int n)
 {
-	int a = 0;
-
-	if (n > 0)
+	while (n > 0)
 	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-	 _putchar('\n');
+		_putchar('_');
+		n--;
 	}
-	else
 	_putchar('\n');
 }
