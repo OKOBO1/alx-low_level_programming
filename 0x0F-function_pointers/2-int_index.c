41b89afe1194a953cb100b28250a052a0be6bee2
OKOBO1 <ugofabian01@gmail.com>
2022-10-13 03:19:05 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,32 +1,28 @@
-#include <stdio.h>
 #include "function_pointers.h"
+
 /**
- * int_index - searches for an integer.
- * @array: Name passed
- * @size: Number of iterations
- * @cmp: Points to a function
- * Return: Always 0 (success)
-**/
+ * int_index - searches for an integer
+ * @array: input integer array.
+ * @size: size of the array.
+ * @cmp: pointer to the function to be used
+ * to compare values
+ *
+ * Return: index of the first eement for which the cmp
+ * function does not return 0. if no elements matches,
+ * return -1. if size <= 0, return -1.
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	if  (array && cmp)
 	{
-		return (-1);
-	}
+		if (size <= 0)
+			return (-1);
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
-		{
-			return (i);
-		}
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
 	}
 
 	return (-1);
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,1 @@
-0x0F-function_pointers
+This Project was done during Full Stack Software Engineering studies at ALX school. The aim of this is to learn about function pointers in C language.
